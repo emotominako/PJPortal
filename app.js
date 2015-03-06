fd5ee2619f4a41348f7d761b8b370441f77044cd
emoto.minako <emoto.minako@jsys.co.jp>
2015-03-06 15:23:12 +0900

    MER、FSメニューから画面切り替えが可能となるよう対応

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var path = require('path');
+var routes = require('./routes/get/index');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('.html', require('ejs').__express);
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.get('/', routes);
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.use('/', express.static(__dirname + "/view/common"));
+app.use('/images', express.static(__dirname + "/public/images"));
+app.use("/bower_components", express.static(__dirname + "/routes/javascript/bower_components"));
+app.use("/dist",express.static(__dirname + "/routes/javascript/dist"));
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(3000);
+
+console.log('Server running at port 3000');
+
+module.exports = app;
