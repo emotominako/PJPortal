1833162bda2fac05a74107394346ce1149953ab8
practice101 <09594@HA1404-2477.jsys.co.jp>
2015-03-11 18:49:41 +0900

    Updated Angular.js sample page to accept number, date & boolean data types

---
+++ routes/javascript/js/app.js
@@ -0,0 +1,38 @@
+var app = angular.module("app", ["xeditable", "ui.bootstrap"]);
+
+app.run(function(editableOptions) {
+  editableOptions.theme = 'bs3';
+});
+
+app.controller('Ctrl', function($scope, $http) {
+  var name = 'awesome user';
+  
+  $scope.user = {
+    id: 3,
+    name: name,
+	employeeNumber : 09594,
+	birthday : new Date(1992,09,13),
+	skills : {type : String, enum : ['java programming', 'c# programming', 'salesforce programming']},
+	hasWorkExperience : false
+  };
+  
+  $scope.updateName = function(){
+	  return $http.post('/?f=name&v=' + $scope.user.name);
+  };
+  
+  $scope.updateEmpNo = function(){
+	  return $http.post('/?f=employeeNumber&v=' + $scope.user.employeeNumber);
+  };
+  
+  $scope.updateBirthday = function(){
+	  return $http.post('/?f=birthday&v=' + $scope.user.birthday);
+  };
+  
+  /*$scope.updateSkills = function(){
+	  return $http.post('/?f=skills&v=' + $scope.user.skills);
+  };*/
+  
+  $scope.updateWorkExperience = function(){
+	  return $http.post('/?f=hasWorkExperience&v=' + $scope.user.hasWorkExperience);
+  };
+});
